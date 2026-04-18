9deee8a8bdbbde06f03a007e7472ce865dccc491
Dayrennn <raflyramandha789@gmail.com>
2026-04-18 12:22:36 +0700

    [ FIX ] - set up

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header("Location: /ecommerce-opencart/upload/");
+exit;
--- upload/useradmin/config.php
+++ upload/useradmin/config.php
@@ -3,17 +3,17 @@
 define('APPLICATION', 'Admin');
 
 // HTTP
-define('HTTP_SERVER', 'http://localhost/opencart-4.1.0.3/upload/useradmin/');
-define('HTTP_CATALOG', 'http://localhost/opencart-4.1.0.3/upload/');
+define('HTTP_SERVER', 'http://localhost/ecommerce-opencart/upload/useradmin/');
+define('HTTP_CATALOG', 'http://localhost/ecommerce-opencart/upload/');
 
 // DIR
-define('DIR_OPENCART', 'C:/xampp/htdocs/opencart-4.1.0.3/upload/');
+define('DIR_OPENCART', '/var/www/html/ecommerce-opencart/upload/');
 define('DIR_APPLICATION', DIR_OPENCART . 'useradmin/');
 define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
 define('DIR_IMAGE', DIR_OPENCART . 'image/');
 define('DIR_SYSTEM', DIR_OPENCART . 'system/');
 define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
-define('DIR_STORAGE', 'C:/xampp/storage/');
+define('DIR_STORAGE', '/var/www/storage_opencart/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -26,11 +26,11 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'ecommerce');
+define('DB_USERNAME', 'rafly');
+define('DB_PASSWORD', 'kukubimaplus');
+define('DB_DATABASE', 'ECommerce');
 define('DB_PORT', '3306');
-define('DB_PREFIX', 'tb_');
+define('DB_PREFIX', 'oc_');
 
 define('DB_SSL_KEY', '');
 define('DB_SSL_CERT', '');
